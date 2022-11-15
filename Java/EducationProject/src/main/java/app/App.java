db997b30dced001b0efeb8592a4cb00858c64951
ALudvick <ludvick@list.ru>
2022-11-15 22:08:11 +0600

    Hide token from java project

--- Java/EducationProject/src/main/java/app/App.java
+++ Java/EducationProject/src/main/java/app/App.java
@@ -6,17 +6,17 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        weatherSimpleRequest();
+        App app = new App();
+        app.weatherSimpleRequest();
     }
 
     // standard method
@@ -44,17 +44,23 @@ public class App {
     }
 
     // get weather method
-    public static void weatherSimpleRequest() throws IOException {
-        // 9346757c649f0798405aca7c1e746a5a
+    public void weatherSimpleRequest() throws IOException {
+        Properties properties = new Properties();
+        properties.load(getClass().getClassLoader().getResourceAsStream("weather.properties"));
+
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(new URL("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=9346757c649f0798405aca7c1e746a5a&units=metric"))
+                .url(new URL("https://api.openweathermap.org/data/2.5/weather?" +
+                        "lat=44.34" +
+                        "&lon=10.99" +
+                        "&appid=" + properties.getProperty("token") +
+                        "&units=metric"))
                 .build();
 
         Response response = client.newCall(request).execute();
         String result = response.body().string();
 
-        System.out.println(result);
+//        System.out.println(result);
 
 //        Pattern pattern = Pattern.compile("\"temp\":(.*?),");
 //        Matcher matcher = pattern.matcher(result);
