e8912098fcf8a7e1f1c63135936e8ecb07be653a
ALudvick <ludvick@list.ru>
2022-11-15 21:35:04 +0600

    Hide token info

--- Python/App/weather/simple/weather.py
+++ Python/App/weather/simple/weather.py
@@ -1,16 +1,16 @@
 import requests as rr
 import re
 import json
+import config
+
 
 # 1. Сформировать вызов API на поиск координат по названию 4 городов (Москва, Астана, Тель-Авив, Санкт-Петербург) -> результат - это координаты (lat и lon)
 # 2. Из полученного ответа координаты (lat и lon) подставить в вызов API погоды и получить температуру по этим 4 городам
 
-
-# 9346757c649f0798405aca7c1e746a5a
 # https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
 
 
-req = rr.request('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=9346757c649f0798405aca7c1e746a5a').text
+req = rr.request('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=' + config.TOKEN).text
 # print(req)
 
 # result = re.search("\"temp\":(.*?),", req)
